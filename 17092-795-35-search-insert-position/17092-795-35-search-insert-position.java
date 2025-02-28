cccb3d69766446411e712b71d6084083512038ca
Kai Huang <harrycheng280505@gmail.com>
2025-02-28 14:59:35 +0800

    Time: 0 ms, Memory: 43.3 MB - LeetHub

---
+++ 17092-795-35-search-insert-position/17092-795-35-search-insert-position.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if (nums == null) return -1;
+        for (int i=0; i<nums.length; i++){
+            if (nums[i] == target) return i;
+        }
+        for (int i=0; i<nums.length; i++){
+            if (nums[i]>= target) return i;
+        }
+        return nums.length;
+    }
+}
