af25de3fb30508acc2b4b185bec7dbb05eb2892a
Cheng Kai Huang <harrycheng280505@gmail.com>
2025-11-18 18:24:32 +0800

    Time: 0 ms (100%), Space: 44.4 MB (10.22%) - LeetHub

--- 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.java
+++ 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.java
@@ -1,6 +1,8 @@
 class Solution {
     public boolean isOneBitCharacter(int[] bits) {
         boolean is2 = false;
+        
+        // handle for case [0]
         if (bits.length == 1) return true;
 
         for (int i=0; i<bits.length; i++){
