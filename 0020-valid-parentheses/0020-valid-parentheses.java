d5eb10fafb73e11e7b8ebb54f418f1d0aa282aa1
Cheng Kai Huang <harrycheng280505@gmail.com>
2026-06-16 19:33:04 +0800

    Time: 3 ms (87.59%), Space: 43.2 MB (56.84%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack <Character> stack = new Stack <>();
+
+        for (char ch: s.toCharArray()){
+            if (ch == '{' || ch == '(' || ch == '['){
+                stack.push(ch);
+            }else{
+                if (stack.isEmpty()) return false;
+                
+                char current = stack.pop();
+                if ((current == '{' && ch != '}') ||
+                    (current == '(' && ch != ')') ||
+                    (current == '[' && ch != ']')){
+                        return false;
+                    }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
